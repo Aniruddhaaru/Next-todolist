8794e65774ac7364bcdeb0e865017c36c4cbe675
Aniruddha <aniruddhaaru06@gmail.com>
2024-06-11 16:33:38 +0530

    done

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import store from '../redux/store';
+// import React from 'react';
+// import { Provider } from 'react-redux';
+// import store from '../redux/store';
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  );
-}
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <Provider store={store}>
+//       <Component {...pageProps} />
+//     </Provider>
+//   );
+// }
 
-export default MyApp;
+// export default MyApp;
